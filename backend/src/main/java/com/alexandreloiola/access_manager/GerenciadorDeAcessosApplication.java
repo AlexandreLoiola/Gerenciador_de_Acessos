ed51dcc0673ddcd51184c59265dec98442fdb5f5
Alexandre Loiola <alexandre.loiola.741@gmail.com>
2023-10-13 23:30:44 -0300

    chore: Inicializacao do backend

---
+++ backend/src/main/java/com/alexandreloiola/access_manager/GerenciadorDeAcessosApplication.java
@@ -0,0 +1,13 @@
+package com.alexandreloiola.access_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeAcessosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeAcessosApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/alexandreloiola/access_manager/GerenciadorDeAcessosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexandreloiola.access_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeAcessosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
